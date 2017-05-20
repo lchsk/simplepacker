eaf14e84ca3e2d49fe2648e124897d1eed143795
lchsk <mjlechowski@gmail.com>
2017-05-20 14:01:59 +0100

    Refactor algorithm.py and add logging

--- simplepacker/algorithm.py
+++ simplepacker/algorithm.py
@@ -1,36 +1,63 @@
 from PIL import Image
-from colours import colours
+from utility import Color
 import files
+from utility import Logger, Color
+
+logger = Logger(__name__)
+
 
 class PackingAlgorithm(object):
-    def __init__(self, settings, file_manager):
-        self.settings = settings
-        self.file_manager = file_manager
-        self.output = None
-        self.errors = False
-        self.record = {}
+    def __init__(self, args, file_manager):
+        self._args = args
+        self._file_manager = file_manager
+        self._output = None
+        self._errors = False
+        self._record = {}
 
-    def prepare(self):
-        self.output = Image.new("RGBA", self.settings.params['output_size'], None)
 
-    def print_output(self):
-        if self.errors:
-            print(colours.FAIL + 'Packing completed with some errors.' + colours.ENDC)
-        else:
-            print(colours.OKGREEN + 'Packing completed successfully!' + colours.ENDC)
+    def run(self):
+        self._prepare()
+        self._pack()
+        self._close()
 
-    def check_for_errors(self, f, i, j):
-        if i >= self.settings.params['output_size'][0] or j >= self.settings.params['output_size'][1]:
-            self.errors = True
-            print(colours.FAIL + 'ERROR: file ' + f + " didn't fit." + colours.ENDC)
 
-    def close(self):
-        self.output.save(self.settings.params['output'])
-        self.print_output()
-        files.save_output(
-            self.settings.params['output'],
-            self.record,
-            self.settings.params['internal_path']
+    def _pack(self):
+        """Must be overriden in child classes"""
+
+        raise NotImplementedError()
+
+
+    def _prepare(self):
+        self._output = Image.new(
+            "RGBA",
+            (self._args.width, self._args.height),
+            None,
         )
 
-        print( colours.OKBLUE + 'Output should be available in ' + self.settings.params['output'] + ' and ' + self.settings.params['output']  + '.txt' + colours.ENDC)
+
+    def _print_output(self):
+        if self._errors:
+            logger.warning('Packing completed with some errors')
+        else:
+            logger.info('Packing completed successfully!', Color.OKGREEN)
+
+
+    def _check_for_errors(self, f, i, j):
+        if i >= self._args.width or j >= self._args.height:
+            self._errors = True
+
+            logger.error('File "{f}" did not fit')
+
+
+    def _close(self):
+        self._output.save(self._args.output)
+        self._print_output()
+        files.save_output(
+            self._args.output,
+            self._record,
+            './'
+            # self.settings.params['internal_path']
+        )
+
+        logger.info('Output is available in "{}"'.format(self._args.output),
+                    Color.OKGREEN)
