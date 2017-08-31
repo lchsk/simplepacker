b2837cc0f7c4c33b2364acef252c7bd6dbc8705d
Maciej Lechowski <mjlechowski@gmail.com>
2017-09-01 00:29:27 +0100

    Repr Rectangle objects

--- simplepacker/geometry.py
+++ simplepacker/geometry.py
@@ -10,6 +10,14 @@ class Rect(object):
         self.bottom = min(p1.y, p2.y)
         self.top    = max(p1.y, p2.y)
 
+    def __repr__(self):
+        return 'Rectangle(%d, %d, %d, %d)' % (
+            self.left,
+            self.bottom,
+            self.right,
+            self.top,
+        )
+
 def overlap(r1, r2):
     overlap_h = True
     overlap_v = True
@@ -18,3 +26,4 @@ def overlap(r1, r2):
     if (r1.top < r2.bottom) or (r1.bottom > r2.top):
         overlap_v = False
     return overlap_h and overlap_v
+
