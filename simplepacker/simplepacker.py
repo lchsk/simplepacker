660bf0b5a35d22be9af94d53728c59785e4daf0f
Maciej Lechowski <mjlechowski@gmail.com>
2017-09-02 14:43:01 +0100

    Use main() as an entry point

--- simplepacker/simplepacker.py
+++ simplepacker/simplepacker.py
@@ -2,10 +2,14 @@ from .settings import read_args
 from .files import FileManager
 from .algorithm_greedy import AlgorithmGreedy
 
-if __name__ == '__main__':
+def main():
     args = read_args()
 
     fm = FileManager(args)
 
     a = AlgorithmGreedy(args, fm)
     a.run()
+
+
+if __name__ == '__main__':
+    main()
