d86c27b3c898cec0b409ccbce6f53eddd642203f
Maciej Lechowski <mjlechowski@gmail.com>
2017-09-02 13:38:08 +0100

    Add a test to check if .params files are created

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import unittest
 
@@ -8,6 +9,8 @@ except ImportError:
 
 from simplepacker.settings import read_args
 from simplepacker.files import FileManager
+from simplepacker.algorithm_greedy import AlgorithmGreedy
+from .common import get_output, clean_output, get_files, group_files
 
 
 class TestTestData(unittest.TestCase):
@@ -18,11 +21,29 @@ class TestTestData(unittest.TestCase):
     def setUp(self):
         super(TestTestData, self).setUp()
 
-        sys.argv = ['simplepacker', '-i', self.TEST_DATA]
-
-        self._args = read_args()
-
-        self._fm = FileManager(self._args)
 
     def test_loading_files(self):
-        self.assertEqual(list(self._fm._files_sorted), ['cat2.jpg', 'cat1.jpg'])
+        sys.argv = ['simplepacker', '-i', self.TEST_DATA]
+
+        args = read_args()
+        fm = FileManager(args)
+
+        self.assertEqual(list(fm._files_sorted), ['cat2.jpg', 'cat1.jpg'])
+
+
+    def test_params_files_creation(self):
+        sys.argv = ['simplepacker', '-i', self.TEST_DATA, '--create-params-files']
+
+        expected = set(['cat1.jpg', 'cat1.jpg.params', 'cat2.jpg', 'cat2.jpg.params'])
+
+        self.assertFalse(expected.issubset(set(os.listdir(self.TEST_DATA))))
+
+        try:
+            _ = FileManager(read_args())
+        except SystemExit:
+            self.assertTrue(expected.issubset(set(os.listdir(self.TEST_DATA))))
+        else:
+            self.fail()
+
+        # Clean up after this test
+        os.remove(self.TEST_DATA + 'cat2.jpg.params')
